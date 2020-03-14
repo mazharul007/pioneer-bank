0a3cf55dd203eab0e483621c9745cc5bf94d8f18
unknown <mohammad.mazharul.hoque7@gmail.com>
2020-03-14 11:21:50 +0600

    sign up section added

---
+++ script.js
@@ -0,0 +1,9 @@
+//event listener for entre button
+const submitBtn = document.getElementById("login");
+submitBtn.addEventListener("click",()=>{
+    //loginArea 
+    const loginArea = document.getElementById("login-area");
+    loginArea.style.display="none";
+});
+
+
