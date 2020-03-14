70f3bd78f1bba4923e20c298e5d87a1bf60564c0
unknown <mohammad.mazharul.hoque7@gmail.com>
2020-03-14 13:56:13 +0600

    deposit button event handler added

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-//event listener for entre button
+//login button event handler
 const submitBtn = document.getElementById("login");
 submitBtn.addEventListener("click",()=>{
-    
+
     //loginArea 
     const loginArea = document.getElementById("login-area");
     loginArea.style.display="none";
@@ -10,5 +10,32 @@ submitBtn.addEventListener("click",()=>{
     const transactionArea = document.getElementById("transaction-area");
     transactionArea.style.display="block";
 });
+
+//deposit button event handler
+const depositBtn = document.getElementById("add-deposit");
+depositBtn.addEventListener("click",()=>{
+    // deposit section
+    const depositAmount = document.getElementById("deposit-amount").value;
+    const depositNumber = parseFloat(depositAmount);
+
+    //deposit section update
+    updateSpanText("currentDeposit",depositNumber);
+    document.getElementById("deposit-amount").value="";
+
+     // balance section 
+    updateSpanText("currentBalance", depositNumber)
+
+    //function for updating balance and deposit 
+
+    function updateSpanText(id, depositNumber){
+        const current = document.getElementById(id).innerText;
+        const currentAmount = parseFloat(current);
+        const totalAmount = depositNumber + currentAmount;
+        document.getElementById(id).innerText= totalAmount;
+    }
+    
+})
+
+
 
 
