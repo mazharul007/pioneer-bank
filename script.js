d0d7987c873118c6f6f2b35e6764477d46493ad4
unknown <mohammad.mazharul.hoque7@gmail.com>
2020-03-14 12:18:17 +0600

    transaction section added

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 //event listener for entre button
 const submitBtn = document.getElementById("login");
 submitBtn.addEventListener("click",()=>{
+    
     //loginArea 
     const loginArea = document.getElementById("login-area");
     loginArea.style.display="none";
+
+    //transaction area
+    const transactionArea = document.getElementById("transaction-area");
+    transactionArea.style.display="block";
 });
 
 
